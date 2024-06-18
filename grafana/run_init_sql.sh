a032a89186e57001a92f627252f24790f662ee8d
huafengchun <huafengchun@gmail.com>
2024-06-18 22:23:59 +0800

    Use amap instead of OSM

---
+++ grafana/run_init_sql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 等待PostgreSQL服务启动
+until pg_isready -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U "$DATABASE_USER"; do
+  echo "Waiting for PostgreSQL to start..."
+  sleep 2
+done
+
+# 执行初始化SQL脚本
+PGPASSWORD=$DATABASE_PASS psql -h "$DATABASE_HOST" -U "$DATABASE_USER" -d "teslamate" -f /proc.sql
+
+# 启动Grafana
+/run.sh
